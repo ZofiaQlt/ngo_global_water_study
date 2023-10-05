45fec0da349e2215cc43bd25e1425b0267515e77
Zofia Quillet <67431758+ZofiaQlt@users.noreply.github.com>
2023-10-05 04:31:13 +0200

    Update fonction_acp.py

--- lib/fonction_acp.py
+++ lib/fonction_acp.py
@@ -22,15 +22,15 @@ def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0,
             # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
             if pcs.shape[1] < 30 :
                 plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
-                   pcs[d1,:], pcs[d2,:], 
+                   pcs[d1,:], pcs[d2,:],
                    angles='xy', scale_units='xy', scale=1, color="grey")
                 # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
             else:
                 lines = [[[0,0],[x,y]] for x,y in pcs[[d1,d2]].T]
                 ax.add_collection(LineCollection(lines, axes=ax, alpha=.1, color='black'))
             
-            # affichage des noms des variables  
-            if labels is not None:  
+            # affichage des noms des variables
+            if labels is not None:
                 for i,(x, y) in enumerate(pcs[[d1,d2]].T):
                     if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
                         plt.text(x, y, labels[i], fontsize='14', ha='center', va='center', rotation=label_rotation, color="blue", alpha=0.5)
@@ -75,7 +75,7 @@ def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, 
             if labels is not None:
                 for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
                     plt.text(x, y, labels[i],
-                              fontsize='12', ha='center',va='center') 
+                              fontsize='12', ha='center',va='center')
                 
             # détermination des limites du graphique
             boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
